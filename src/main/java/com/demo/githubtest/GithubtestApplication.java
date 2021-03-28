4bfbc82d35155732cca63942617ea35b3241ffad
Raushan Kumar <raushankumar2788@gmail.com>
2021-03-28 20:40:29 +0530

    1st

---
+++ src/main/java/com/demo/githubtest/GithubtestApplication.java
@@ -0,0 +1,15 @@
+package com.demo.githubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubtestApplication {
+
+	public static void main(String[] args) {
+	System.out.println("kumar");
+	
+		SpringApplication.run(GithubtestApplication.class, args);
+	}
+
+}
