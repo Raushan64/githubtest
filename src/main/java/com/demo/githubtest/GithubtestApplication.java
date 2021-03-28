a1178316c10ed5c461fc983cd275defc595fa572
Raushan Kumar <raushankumar2788@gmail.com>
2021-03-28 20:45:13 +0530

    changes
    In this I changed kumar to singh.all the best

--- src/main/java/com/demo/githubtest/GithubtestApplication.java
+++ src/main/java/com/demo/githubtest/GithubtestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubtestApplication {
 
 	public static void main(String[] args) {
-	System.out.println("kumar");
+	System.out.println("singh");
 	
 		SpringApplication.run(GithubtestApplication.class, args);
 	}
